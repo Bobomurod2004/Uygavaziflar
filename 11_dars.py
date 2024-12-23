24aff3d02a125ef1e00cd055e7243859b592f6d0
Bobomurod2004 <bobomurodgoibnazarov@gmail.com>
2024-12-23 10:49:22 +0500

    Add files via upload

---
+++ 11_dars.py
@@ -0,0 +1,14 @@
+# def yigindi(son):
+#     summ=0
+#     while son>0:
+#         summ +=son%10
+#         son//=10
+#     return  summ
+# son=int(input("son kiriting="))
+# print(f"Biz kiritgan son yig'indis={yigindi(son)}")
+#2masala--------------------------------------------
+# my_list=['olma','olcha','uzmum']
+# katta=list(map(lambda x:str(x).upper(),my_list))
+# print(katta)
+#1masala---------------------
+
